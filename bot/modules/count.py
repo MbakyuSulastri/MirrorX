aec58cd2698d011b8d50706b0c47e0317bef3d28
MbakyuSulastri <70559883+MbakyuSulastri@users.noreply.github.com>
2021-06-23 10:39:01 +0700

    Update count.py

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -20,7 +20,7 @@ def countNode(update,context):
         else:
             uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
         if uname is not None:
-            cc = f'\n\nReq. By: {uname}'
+            cc = f'\n\nAdded By: {uname}'
         sendMessage(result + cc, context.bot, update)
     else:
         sendMessage("Provide G-Drive Shareable Link to Count.",context.bot,update)
